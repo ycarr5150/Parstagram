a795ae0b16b05a2d70144e1fa2b72038100c55ec
Yazmin Carrillo <ycarrillo@csumb.edu>
2020-03-04 00:05:11 -0800

    First commit

---
+++ Parstagram/PostTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostTableViewCell.swift
+//  Parstagram
+//
+//  Created by Yazmin Carrillo on 3/3/20.
+//  Copyright © 2020 Yazmin Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
